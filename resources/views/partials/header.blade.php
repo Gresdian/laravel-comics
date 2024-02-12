f3314837e8edbdd4adc2c93e9da4201e30a4a24a
Gresdian <chris_da98@outlook.it>
2024-02-12 18:27:31 +0100

    [update] add partials, img folder and completed header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,49 @@
+<header>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <a href="/">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC comics">
+                </a>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="#">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP ▾</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="search-bar">
+                <input class="text-end border-bottom border-primary border-3 searchbar" type="search" name=""
+                    value="" placeholder="Search">
+            </div>
+        </div>
+    </div>
+</header>
